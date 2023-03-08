fb7b8c3050be6f485b7e1ec2bd75bf50d410902f
Vikram Sehra <vikramkr.meena.min16@itbhu.ac.in>
2023-03-08 11:38:46 +0530

    Host API live

---
+++ app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const connectDB = require("./db/connect")
+
+const PORT = process.env.PORT || 5000;
+
+const products_routes = require("./routes/Products")
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am live");
+});
+
+app.use("/api/products", products_routes)
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO);
+        app.listen(PORT, () => {
+            console.log(`${PORT} yes i am connected`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
